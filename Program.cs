b7887875c88f6be78d9445e5bd5305da1a7da905
Harut Safaryan <safaryanharut@gmail.com>
2021-12-18 10:22:29 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.Print();
+            Console.ReadLine();
+        }
+    }
+}
